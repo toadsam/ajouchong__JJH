a8aa114ba664c036a2d2d3ff1c1ecc5c3f82d2a9
y.tnwjd <ysjsw@ajou.ac.kr>
2024-11-22 16:29:56 +0900

    chore : change api endpoint & return value

--- src/main/java/com/ajouchong/controller/user/OAuthController.java
+++ src/main/java/com/ajouchong/controller/user/OAuthController.java
@@ -4,19 +4,20 @@ import com.ajouchong.common.ApiResponse;
 import com.ajouchong.oauth.OAuth2Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value ="/api/login/oauth", produces = "application/json")
 @RequiredArgsConstructor
-public class AuthController {
+public class OAuthController {
+
     private final OAuth2Service oAuth2Service;
 
-    @GetMapping("/dev/login/oauth/{registration}")
-    public ApiResponse<?> devSocialLogin(@RequestParam String code, @PathVariable String registration) {
-        String loginMessage = oAuth2Service.devSocialLogin(code);
-        return new ApiResponse<>(1,  loginMessage, null);
+    @GetMapping("/google")
+    public ApiResponse<String> loginWithGoogle(@RequestParam String code) {
+        return oAuth2Service.socialLogin(code);
     }
 }
 
