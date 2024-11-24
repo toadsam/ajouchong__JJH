dd2df1f24376a34b1e4bf1e497913b3b55e84310
y.tnwjd <ysjsw@ajou.ac.kr>
2024-11-25 00:58:04 +0900

    feat: add profile response dto

--- src/main/java/com/ajouchong/dto/response/ProfileResponseDto.java
+++ src/main/java/com/ajouchong/dto/response/ProfileResponseDto.java
@@ -1,2 +1,14 @@
-package com.ajouchong.dto.response;public class ProfileResponseDto {
+package com.ajouchong.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProfileResponseDto {
+    private String name;
+    private String email;
+    private String role;
 }
