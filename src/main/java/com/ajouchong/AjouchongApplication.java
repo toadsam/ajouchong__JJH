84ead220855d0d85a559aa84cd155f627c58f0ef
y.tnwjd <ysjsw@ajou.ac.kr>
2024-11-26 09:33:36 +0900

    chore: rm feign annotation

--- src/main/java/com/ajouchong/AjouchongApplication.java
+++ src/main/java/com/ajouchong/AjouchongApplication.java
@@ -3,10 +3,8 @@ package com.ajouchong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableFeignClients(basePackages = "com.ajouchong.oauth")
 public class AjouchongApplication {
 
 	public static void main(String[] args) {
