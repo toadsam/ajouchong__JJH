5685efb2343bd01bafeb06a56308550f5815f6e5
y.tnwjd <ysjsw@ajou.ac.kr>
2024-11-27 11:28:58 +0900

    chore: login after redirect

--- src/main/java/com/ajouchong/controller/user/OAuthController.java
+++ src/main/java/com/ajouchong/controller/user/OAuthController.java
@@ -1,7 +1,7 @@
 package com.ajouchong.controller.user;
 
-import com.ajouchong.common.ApiResponse;
 import com.ajouchong.oauth.OAuth2Service;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +9,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value ="/api/login/oauth", produces = "application/json")
+@RequestMapping(value = "/api/login/oauth", produces = "application/json")
 @RequiredArgsConstructor
 public class OAuthController {
 
     private final OAuth2Service oAuth2Service;
 
     @GetMapping("/google")
-    public ApiResponse<String> loginWithGoogle(@RequestParam String code) {
-        return oAuth2Service.socialLogin(code);
+    public void loginWithGoogle(@RequestParam String code, HttpServletResponse response) {
+        String jwtToken = oAuth2Service.socialLogin(code).getData();
+        response.setHeader("Authorization", "Bearer " + jwtToken);
+
+        // ajouchong.com으로 리다이렉트
+        response.setStatus(HttpServletResponse.SC_FOUND);
+        response.setHeader("Location", "https://ajouchong.com");
     }
 }
 
