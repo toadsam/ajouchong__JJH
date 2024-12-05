512ca5d4709ad2bb8a085ce1e1b4a8be535af30a
y.tnwjd <ysjsw@ajou.ac.kr>
2024-12-06 02:19:46 +0900

    chore: add allowOrigin URL

--- src/main/java/com/ajouchong/config/SecurityConfig.java
+++ src/main/java/com/ajouchong/config/SecurityConfig.java
@@ -53,7 +53,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://www.ajouchong.com"
+        ));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
---
+++ src/main/java/com/ajouchong/config/WebMvcConfig.java
@@ -0,0 +1,4 @@
+package com.ajouchong.config;
+
+public class AttachmentConfig {
+}
